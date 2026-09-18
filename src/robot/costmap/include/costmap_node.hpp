690645cce6cdd059283e5b2e2aa6682089be69f2
yichenxiao <yichenxiao08@gmail.com>
2026-09-18 16:04:04 +0800

    init

--- src/robot/costmap/include/costmap_node.hpp
+++ src/robot/costmap/include/costmap_node.hpp
@@ -2,6 +2,7 @@
 #define COSTMAP_NODE_HPP_
 
 #include "rclcpp/rclcpp.hpp"
+#include "std_msgs/msg/string.hpp"
 
 #include "costmap_core.hpp"
 
@@ -9,8 +10,13 @@ class CostmapNode : public rclcpp::Node {
   public:
     CostmapNode();
 
+    void publishMessage();
+
   private:
     robot::CostmapCore costmap_;
+
+    rclcpp::Publisher<std_msgs::msg::String>::SharedPtr string_pub_;
+    rclcpp::TimerBase::SharedPtr timer_;
 };
 
 #endif 
--- src/robot/costmap/src/costmap_node.cpp
+++ src/robot/costmap/src/costmap_node.cpp
@@ -3,9 +3,21 @@
 
 #include "costmap_node.hpp"
 
-CostmapNode::CostmapNode() : Node("costmap"), costmap_(robot::CostmapCore(this->get_logger())) {}
+CostmapNode::CostmapNode() : Node("costmap"), costmap_(robot::CostmapCore(this->get_logger()))
+{
+  string_pub_ = this->create_publisher<std_msgs::msg::String>("/test_topic", 10);
+  timer_ = this->create_wall_timer(std::chrono::milliseconds(500), std::bind(&CostmapNode::publishMessage, this));
+}
 
-int main(int argc, char ** argv)
+void CostmapNode::publishMessage()
+{
+  auto message = std_msgs::msg::String();
+  message.data = "Hello, ROS 2!";
+  RCLCPP_INFO(this->get_logger(), "Publishing : '%s'", message.data.c_str());
+  string_pub_->publish(message);
+}
+
+int main(int argc, char **argv)
 {
   rclcpp::init(argc, argv);
   rclcpp::spin(std::make_shared<CostmapNode>());
